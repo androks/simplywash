170917b1f89ed20481a483fd84106b9a4c1eb203
androks <bohdan.olynyk@gmail.com>
2017-03-10 13:54:03 +0200

    refactor activities

--- app/src/main/java/androks/simplywash/Activities/ReviewsActivity.java
+++ app/src/main/java/androks/simplywash/Activities/ReviewsActivity.java
@@ -1,29 +1,36 @@
 package androks.simplywash.Activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import androks.simplywash.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ReviewsActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reviews);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setUptolbar();
+
+    }
+
+    private void setUptolbar() {
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                onBackPressed();
             }
         });
     }
