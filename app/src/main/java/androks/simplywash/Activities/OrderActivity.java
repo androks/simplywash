f412ced1f8f3c4761dc6085e1053c2b9aad6562f
androks <bohdan.olynyk@gmail.com>
2017-03-15 23:27:44 +0200

    Pick date and time works property

--- app/src/main/java/androks/simplywash/Activities/OrderActivity.java
+++ app/src/main/java/androks/simplywash/Activities/OrderActivity.java
@@ -1,12 +1,16 @@
 package androks.simplywash.Activities;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -23,6 +27,7 @@ import java.util.Map;
 
 import androks.simplywash.Constants;
 import androks.simplywash.Enums.Day;
+import androks.simplywash.Models.Service;
 import androks.simplywash.Models.Washer;
 import androks.simplywash.R;
 import androks.simplywash.Utils;
@@ -32,18 +37,31 @@ import butterknife.OnClick;
 
 public class OrderActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar) Toolbar toolbar;
-    @BindView(R.id.date) Spinner dateSpinner;
-    @BindView(R.id.carTypesSpinner) Spinner carTypesSpinner;
-    @BindView(R.id.price) TextView price;
-    @BindView(R.id.servicesRecycleView) RecyclerView servicesRecycleView;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.date)
+    Spinner dateSpinner;
+    @BindView(R.id.carTypesSpinner)
+    Spinner carTypesSpinner;
+    @BindView(R.id.price)
+    TextView price;
+    @BindView(R.id.servicesRecycleView)
+    RecyclerView servicesRecycleView;
+    @BindView(R.id.timeBtn)
+    Button timeBtn;
 
-
-    private List<String> availableTimes = new ArrayList<>();
-    private Map<String, Map<String, ArrayList<String>>> busyTimes;
     private String washerId;
     private Day selectedDay;
     private Washer washer;
+    private String selectedTime;
+
+    private List<String> availableTimes = new ArrayList<>();
+    private Map<String, Map<String, ArrayList<String>>> busyTimes;
+
+    private Map<String, Map<String, Service>> priceList;
+    private List<Service> showingPrices = new ArrayList<>();
+
+    private PriceActivity.PriceListRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,8 +69,8 @@ public class OrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order);
         ButterKnife.bind(this);
         washerId = getIntent().getExtras().getString(Constants.WASHER_ID);
-        inflateSpinner();
-        downloadWasher();
+        downloadBusyTimes();
+        downloadPriceList();
     }
 
     private void downloadWasher() {
@@ -61,7 +79,7 @@ public class OrderActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 washer = dataSnapshot.getValue(Washer.class);
                 setUpToolbar();
-                fillAvailableTimes();
+                inflateSpinner();
             }
 
             @Override
@@ -80,8 +98,11 @@ public class OrderActivity extends AppCompatActivity {
         dateSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                fillAvailableTimes();
                 selectedDay = Day.values()[position];
+                fillAvailableTimes();
+                setUpAvailableTimes();
+                timeBtn.performClick();
+                timeBtn.setText(availableTimes.get(0));
             }
 
             @Override
@@ -92,27 +113,32 @@ public class OrderActivity extends AppCompatActivity {
     }
 
     private void fillAvailableTimes() {
+        availableTimes.clear();
         Calendar calendar = Calendar.getInstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
         int minute = calendar.get(Calendar.MINUTE);
         minute = Utils.castMinuteToFormat(minute);
+        if (dateSpinner.getSelectedItem() != Day.Today) {
+            hour = 0;
+            minute = 0;
+        }
+
 
         while (hour < 24) {
-            while(minute <= 60){
+            while (minute <= 60) {
                 if (minute == 60) {
                     minute = 0;
                     break;
                 }
                 availableTimes.add(String.format(
                         Locale.getDefault(),
-                        String.format(Locale.getDefault(),"%02d:%02d", hour, minute),
+                        String.format(Locale.getDefault(), "%02d:%02d", hour, minute),
                         hour, Utils.castMinuteToFormat(minute)
                 ));
                 minute += 15;
             }
             hour++;
         }
-        downloadBusyTimes();
     }
 
 
@@ -126,11 +152,7 @@ public class OrderActivity extends AppCompatActivity {
                                     <Map<String, Map<String, ArrayList<String>>>>() {
                             }
                     );
-                    for (Map.Entry<String, ArrayList<String>> temp :
-                            busyTimes.get(selectedDay.name()).entrySet()) {
-                        if (temp.getValue().size() >= washer.getBoxes())
-                            availableTimes.remove(temp.getKey());
-                    }
+                    downloadWasher();
                 }
             }
 
@@ -141,6 +163,14 @@ public class OrderActivity extends AppCompatActivity {
         });
     }
 
+    private void setUpAvailableTimes() {
+        if (busyTimes.containsKey(selectedDay.name()))
+            for (Map.Entry<String, ArrayList<String>> temp :
+                    busyTimes.get(selectedDay.name()).entrySet())
+                if (temp.getValue().size() >= washer.getBoxes())
+                    availableTimes.remove(temp.getKey());
+    }
+
     private void setUpToolbar() {
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
@@ -148,8 +178,72 @@ public class OrderActivity extends AppCompatActivity {
         }
     }
 
-    @OnClick({R.id.timeBtn, R.id.time})
-    public void pickTime(){
+    private void downloadPriceList() {
+        Utils.getPricesFor(washerId).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.hasChildren()) {
+                    priceList = dataSnapshot.getValue(
+                            new GenericTypeIndicator<Map<String, Map<String, Service>>>() {
+                            }
+                    );
+                    setUpCarTypesSpinner();
+                    setUpRecyclerView();
+                }
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void setUpCarTypesSpinner() {
+        List<String> carTypes = new ArrayList<>();
+        carTypes.addAll(priceList.keySet());
+        carTypesSpinner.setAdapter(new ArrayAdapter<>(
+                this,
+                R.layout.spinner_dropdown_toolbar_item,
+                carTypes)
+        );
+        carTypesSpinner.setSelection(0);
+        carTypesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                showingPrices.clear();
+                showingPrices.addAll(priceList.get(carTypesSpinner.getSelectedItem()).values());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void setUpRecyclerView() {
+        showingPrices.addAll(priceList.get(carTypesSpinner.getSelectedItem()).values());
+        servicesRecycleView.setHasFixedSize(true);
+        servicesRecycleView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new PriceActivity.PriceListRecyclerAdapter(showingPrices);
+        servicesRecycleView.setAdapter(adapter);
+    }
+
+    @OnClick({R.id.timeBtn, R.id.time})
+    public void pickTime() {
+        String[] values = new String[availableTimes.size()];
+        values = availableTimes.toArray(values);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pick time")
+                .setItems(values, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        selectedTime = availableTimes.get(which);
+                        timeBtn.setText(selectedTime);
+                    }
+                });
+        builder.create().show();
     }
 }
