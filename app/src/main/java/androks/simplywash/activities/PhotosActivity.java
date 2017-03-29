a677bfd7d477856c4f8d997d49da59c17f80a9d2
androks <bohdan.olynyk@gmail.com>
2017-03-29 17:43:16 +0300

    Photos works property

--- app/src/main/java/androks/simplywash/activities/PhotosActivity.java
+++ app/src/main/java/androks/simplywash/activities/PhotosActivity.java
@@ -1,8 +1,6 @@
 package androks.simplywash.activities;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.NavUtils;
@@ -27,58 +25,18 @@ import androks.simplywash.Utils;
 import androks.simplywash.fragments.ImageFragment;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class PhotosActivity extends AppCompatActivity {
-
-    @BindView(R.id.fullscreen_content) View mContentView;
     @BindView(R.id.image_slideshow) ViewPager mPhotosViewPager;
-    @BindView(R.id.progress) View mProgress;
 
     /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
      */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
 
     private String mWasherId;
     private List<StorageReference> mPhotoReferences = new ArrayList<>();
@@ -89,8 +47,6 @@ public class PhotosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_photos);
         ButterKnife.bind(this);
         setUpToolbar();
-
-        mVisible = true;
 
         mWasherId = getIntent().getExtras().getString(Constants.WASHER_ID);
 
@@ -122,26 +78,11 @@ public class PhotosActivity extends AppCompatActivity {
         mPhotosViewPager.setPageTransformer(true, new DepthPageTransformer());
     }
 
-    @OnClick(R.id.fullscreen_content)
-    public void toggle(){
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
-
     private void setUpToolbar() {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-    }
-
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        delayedHide(100);
     }
 
     @Override
@@ -153,49 +94,6 @@ public class PhotosActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    public void showProgress(){
-        mProgress.setVisibility(View.VISIBLE);
-    }
-
-    public void hideProgress(){
-        mProgress.setVisibility(View.GONE);
-    }
-
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
-        hideProgress();
     }
 
     /**
--- app/src/main/java/androks/simplywash/fragments/ImageFragment.java
+++ app/src/main/java/androks/simplywash/fragments/ImageFragment.java
@@ -9,6 +9,9 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.firebase.storage.StorageReference;
 
@@ -24,6 +27,7 @@ import butterknife.Unbinder;
 public class ImageFragment extends Fragment {
 
     @BindView(R.id.image) ImageView image;
+    @BindView(R.id.progress) View progressBar;
 
     private Unbinder unbinder;
 
@@ -38,6 +42,18 @@ public class ImageFragment extends Fragment {
         Glide.with(this)
                 .using(new FirebaseImageLoader())
                 .load(photoReference)
+                .listener(new RequestListener<StorageReference, GlideDrawable>() {
+                    @Override
+                    public boolean onException(Exception e, StorageReference model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(GlideDrawable resource, StorageReference model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
                 .into(image);
         return rootView;
     }
