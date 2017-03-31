51c656d044d99dfa0a9e52a074a03ef77fbe99b5
androks <bohdan.olynyk@gmail.com>
2017-03-31 23:54:18 +0300

    Fixed digits logout

--- app/src/main/java/androks/simplywash/activities/SettingsActivity.java
+++ app/src/main/java/androks/simplywash/activities/SettingsActivity.java
@@ -14,6 +14,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.digits.sdk.android.Digits;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -144,6 +145,7 @@ public class SettingsActivity extends AppCompatActivity {
         edit.putString(Constants.PHONE_PREF, null);
         edit.putString(Constants.CITY_PREF, null);
         edit.apply();
+        Digits.logout();
         startActivity(new Intent(SettingsActivity.this, LoginActivity.class));
         finish();
     }
